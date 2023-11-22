f7a422f33a6d9247916728740cc2a7c2e81783a8
Marko Kask <kaskmarko@gmail.com>
2023-11-22 15:03:44 +0200

    Add model classes.

---
+++ eComics/Data/Enums/BookGenre.cs
@@ -0,0 +1,11 @@
+﻿namespace eComics.Data.Enums
+{
+    public enum BookGenre
+    {
+        Action = 1,
+        Adventure,
+        SciFi,
+        Fantasy,
+        Horror
+    }
+}
---
+++ eComics/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using eComics.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace eComics.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public BookGenre BookGenre { get; set; } 
+
+    }
+}
