a4a6dcb8b93aa2c3049bb848d949fefa28eb2daa
Marko Kask <kaskmarko@gmail.com>
2023-11-24 14:27:47 +0200

    Add IArtistsService interface

---
+++ eComics/Data/Services/IArtistsService.cs
@@ -0,0 +1,13 @@
+﻿using eComics.Models;
+
+namespace eComics.Data.Services
+{
+    public interface IArtistsService
+    {
+        IEnumerable<Artist> GetAll();
+        Artist GetById(int id);
+        void Add(Artist artist);
+        Artist Update(int id, Artist newArtist);
+        void Delete(int id);
+    }
+}
