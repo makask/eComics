d80cd371848846fb735080cb2c05a9b42c5edd6c
Marko Kask <kaskmarko@gmail.com>
2023-11-24 18:33:06 +0200

    Add AddAsync() method in base repository

--- eComics/Data/Base/EntityBaseRepository.cs
+++ eComics/Data/Base/EntityBaseRepository.cs
@@ -12,22 +12,12 @@ namespace eComics.Data.Base
             _context = context; 
         }
 
-        public async Task<IEnumerable<T>> GetAllAsync()
-        {
-            var result = await _context.Set<T>().ToListAsync();
-            return result;
-        }
-
-        public async Task<T> GetByIdAsync(int id)
-        {
-            var result = await _context.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
-            return result;
-        }
-
-        public Task AddAsync(T entity)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+       
+        public async Task<T> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(a => a.Id == id);
+        
+        public async Task AddAsync(T entity) => await _context.Set<T>().AddAsync(entity);
+      
 
         public Task DeleteAsync(int id)
         {
