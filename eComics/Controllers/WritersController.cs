dee6a61663fbc285cfbd75be181a890b689aac22
Marko Kask <kaskmarko@gmail.com>
2023-11-25 14:13:42 +0200

    Add writer index view update, added pagination, search by name, filter by name

--- eComics/Controllers/WritersController.cs
+++ eComics/Controllers/WritersController.cs
@@ -1,5 +1,6 @@
 ﻿using eComics.Data;
 using eComics.Data.Services;
+using eComics.Data.ViewModels;
 using eComics.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.Operations;
@@ -16,10 +17,53 @@ namespace eComics.Controllers
             _service = service;    
         }
 
-        public async Task<IActionResult> Index()
+        /*public async Task<IActionResult> Index()
         {
             var allWriters = await _service.GetAllAsync();
             return View(allWriters);
+        }*/
+
+        public async Task<IActionResult> Index(string term = "", string orderBy = "", int currentPage = 1)
+        {
+            var allWriters = await _service.GetAllAsync();
+
+            term = string.IsNullOrEmpty(term) ? "" : term.ToLower();
+            var writerData = new WriterVM();
+
+            writerData.NameSortOrder = string.IsNullOrEmpty(orderBy) ? "name_desc" : "";
+
+            var writers = (from writer in allWriters
+                           where term == "" || writer.FullName.ToLower().StartsWith(term)
+                           select new Writer
+                           {
+                               Id = writer.Id,
+                               ProfilePictureURL = writer.ProfilePictureURL,
+                               FullName = writer.FullName,
+                               Bio = writer.Bio
+                           });
+
+            switch (orderBy)
+            {
+                case "name_desc":
+                    writers = writers.OrderByDescending(w => w.FullName);
+                    break;
+                default:
+                    writers = writers.OrderBy(w => w.FullName);
+                    break;
+            }
+
+            int totalRecords = writers.Count();
+            int pageSize = 6;
+            int totalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
+            writers = writers.Skip((currentPage - 1) * pageSize).Take(pageSize);
+            writerData.Writers = writers;
+            writerData.CurrentPage = currentPage;
+            writerData.TotalPages = totalPages;
+            writerData.PageSize = pageSize;
+            writerData.Term = term;
+            writerData.OrderBy = orderBy;
+            return View(writerData);
+
         }
 
         public IActionResult Create() 
---
+++ eComics/Data/ViewModels/WriterVM.cs
@@ -0,0 +1,21 @@
+﻿using eComics.Models;
+
+namespace eComics.Data.ViewModels
+{
+    public class WriterVM
+    {
+        public IEnumerable<Writer> Writers { get; set; }
+
+        public string NameSortOrder { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public string Term { get; set; }
+
+        public string OrderBy { get; set; }
+    }
+}
