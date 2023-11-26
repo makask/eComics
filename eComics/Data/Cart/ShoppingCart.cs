a1ec22cf405a6e3a49d20fe65d87e1e6b50715a3
Marko Kask <kaskmarko@gmail.com>
2023-11-26 17:12:22 +0200

    Add setting up ShoppingCart class

---
+++ eComics/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using eComics.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eComics.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems() 
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId ==
+                ShoppingCartId).Include(b => b.Book).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        { 
+            var total = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(p => p.Book.Price * p.Amount).Sum();
+            return total;
+        }
+    }
+}
