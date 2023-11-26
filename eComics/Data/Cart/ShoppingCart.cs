ab49fddb69a70d77057266deb3a4448b57948dc9
Marko Kask <kaskmarko@gmail.com>
2023-11-26 17:27:56 +0200

    Add AddItemToCart method

--- eComics/Data/Cart/ShoppingCart.cs
+++ eComics/Data/Cart/ShoppingCart.cs
@@ -15,6 +15,28 @@ namespace eComics.Data.Cart
             _context = context;
         }
 
+        public void AddItemToCart(Book book)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Book.Id == book.Id &&
+                n.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Book = book,
+                    Amount = 1
+                };
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
+
         public List<ShoppingCartItem> GetShoppingCartItems() 
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId ==
