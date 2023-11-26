73e357ddfd4c7a1d03e4f996b802c0ddac79c985
Marko Kask <kaskmarko@gmail.com>
2023-11-26 20:22:42 +0200

    Add listing all orders

--- eComics/Controllers/OrdersController.cs
+++ eComics/Controllers/OrdersController.cs
@@ -17,6 +17,13 @@ namespace eComics.Controllers
             _ordersService = ordersService;
         }
 
+        public async Task <IActionResult> Index()
+        {
+            string userId = "";
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
